ce99230adbf066409ca698cab12cdd6defbcd5be
angellohor <Angel.Lopez30@alu.uclm.es>
2025-06-03 18:08:37 +0200

    face_recognition

--- run.py
+++ run.py
@@ -10,7 +10,10 @@ import keyboard # pip install keyboard
 from capturing import VirtualCamera
 from overlays import initialize_hist_figure, plot_overlay_to_image, plot_strings_to_image, update_histogram
 from basics import histogram_figure_numba
-
+import face_recognition
+import os
+import cv2 
+import numpy as np
 
 # Example function
 # You can use this function to process the images from opencv
@@ -18,11 +21,59 @@ from basics import histogram_figure_numba
 def custom_processing(img_source_generator):
     # use this figure to plot your histogram
     fig, ax, background, r_plot, g_plot, b_plot = initialize_hist_figure()
+    # If you want to use face recognition, load the model here
+    known_face_encodings = []
+    known_face_names = []
+    # Load your known faces here
+    pictures_dir = "Virtual-Camera\images"
+    for picture in os.listdir(pictures_dir):
+        if picture.endswith(".jpg") or picture.endswith(".png"):
+            image = face_recognition.load_image_file(os.path.join(pictures_dir, picture))
+            encoding = face_recognition.face_encodings(image)[0]
+            if encoding is not None:
+                known_face_encodings.append(encoding)
+                known_face_names.append(picture.split('.')[0])
+
     
     for sequence in img_source_generator:
         # Call your custom processing methods here! (e. g. filters)
-        
-        
+        small_frame = cv2.resize(sequence, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = []
+        if face_locations:
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_names = []
+        for face_encoding in face_encodings:
+            # Compare the face encoding with the known faces
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            # If a match was found, use the first match
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = known_face_names[first_match_index]
+
+            face_names.append(name)
+
+        sequence = np.ascontiguousarray(sequence, dtype=np.uint8)
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            if name == "Unknown":
+                face = sequence[top:bottom, left:right]
+                if face.size != 0:
+                    face_blur = cv2.GaussianBlur(face, (51, 51), 0)
+                    face_blur = cv2.GaussianBlur(face, (51, 51), 0)
+                    sequence[top:bottom, left:right] = face_blur
+            
+
+            cv2.rectangle(sequence, (left, top), (right, bottom), (0, 255, 0), 2)
+            cv2.putText(sequence, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
         # Example of keyboard is pressed
         # If you want to use this method then consider implementing a counter
@@ -57,6 +108,7 @@ def custom_processing(img_source_generator):
         
         # Make sure to yield your processed image
         yield sequence
+
 
 
 
